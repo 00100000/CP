1d84f5a3726c427c5a723aad4f6b74416589f68b
Aidan Din <00100000spacebar@gmail.com>
2026-08-23 22:03:14 -0400

    Update and rename switch_from_clang_to_gcc.sh to macos_setup.sh

--- include/macos_setup.sh
+++ include/macos_setup.sh
@@ -1,9 +1,12 @@
+# this doesn't actually work as a .sh script right now... I might change that later
+# MacOS SIP messes up algo/debug.h every update, so this fixes that
+
 # 1. Install gcc
 brew install gcc
 
 # 2. Symlink g++ to gcc instead of clang++
 cd /opt/homebrew/bin/
-ln -s g++-14 g++ (WARNING: Change version of g++ to the version installed in step 1. e.g. 'g++-15' instead of 'g++-12')
+ln -s g++-15 g++ # (WARNING: Change version of g++ to the version installed in step 1. e.g. 'g++-17')
 
 # 3. Find include path (top non-ignored entry after running this command)
 echo | g++ -E -Wp,-v -
